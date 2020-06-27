eca20c39c8d996510d55d8e336170b35cc502272
zhanjunrui <50127580+zhanjunrui@users.noreply.github.com>
2020-06-27 16:36:15 +0800

    20200627 git commit

--- src/test/java/com/zhanjunrui/test/GoodTest01.java
+++ src/test/java/com/zhanjunrui/test/GoodTest01.java
@@ -2,7 +2,7 @@ package com.zhanjunrui.test;
 
 public class GoodTest01 {
 //git commit test 0627 01
-//git commit tset 0627 02 haha
+//git commit test 0627 02 haha
 // git commit test 0627 03 wawa
-
+// git commit test 04
 }
